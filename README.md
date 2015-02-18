870c6db3e0125a12e794497aa92420415ea04442
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-17 16:06:56 -0800

    Add svg drawing capabilities

---
+++ README.md
@@ -0,0 +1,13 @@
+# Package Price
+This is an app that allows the user to package dimensions and weight and it calculates the price.
+## Setup Instructions:
+```
+Clone repository and open the ups.html file.
+```
+### Bug Reports:
+```
+None known
+```
+### Author:
+Alex Kaufman and Grace Mekarski
+#### MIT License
--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,20 @@ $(document).ready(function(){
                       weight: inputtedWeight, height: inputtedHeight,
                       price: function() {
                         return this.length + this.width + this.height * this.weight;
+                      },
+                      draw: function(){
+                        return "<svg width='"
+                        + (this.width * 10)
+                        + "' height='"
+                        + (this.height * 10)
+                        + "'><rect width='"
+                        + (this.width * 10)
+                        + "' height='"
+                        + (this.height * 10)
+                        + "'style='fill:rgb(164,124,72);stroke-width:3;stroke:rgb(0,0,0)'/></svg>"
                       }
                       };
+    // var draw = "<svg width=" + newPackage.width +
 
     $("input#new-package-length").val("");
     $("input#new-package-width").val("");
@@ -24,5 +36,10 @@ $(document).ready(function(){
     $(".width").text(newPackage.width);
     $(".height").text(newPackage.height);
     $(".weight").text(newPackage.weight);
+    $(".draw").empty();
+    $(".draw").append(newPackage.draw());
+
+
+
   });
 });
